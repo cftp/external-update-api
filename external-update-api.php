aed861df3aafde28b05e116ce13045003e591f0e
John Blackbourn <johnbillion@gmail.com>
2014-04-26 00:43:05 +0100

    Important bugfix

--- external-update-api.php
+++ external-update-api.php
@@ -13,6 +13,23 @@ Network:      true
 
 Copyright © 2014 Code for the People Ltd
 
+            _____________
+           /      ____   \
+     _____/       \   \   \
+    /\    \        \___\   \
+   /  \    \                \
+  /   /    /          _______\
+ /   /    /          \       /
+/   /    /            \     /
+\   \    \ _____    ___\   /
+ \   \    /\    \  /       \
+  \   \  /  \____\/    _____\
+   \   \/        /    /    / \
+    \           /____/    /___\
+     \                        /
+      \______________________/
+
+
 This program is free software; you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
 the Free Software Foundation; either version 2 of the License, or
